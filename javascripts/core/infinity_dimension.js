4a02111ec2da1de6041ad9544a8268e3e674d067
mathisrvfan <98718993+mathisrvfan@users.noreply.github.com>
2023-01-28 15:02:34 -0500

    Update infinity_dimension.js
    hopefully will not f up

--- javascripts/core/infinity_dimension.js
+++ javascripts/core/infinity_dimension.js
@@ -75,6 +75,7 @@ function DimensionPower(tier) {
   mult = mult.times(infDimPow)
 
   mult = mult.times(kongAllDimMult)
+  mult = Decimal.pow(mult,17)
   if (player.achievements.includes("r94") && tier == 1) mult = mult.times(2);
   if (player.achievements.includes("r75")) mult = mult.times(player.achPow);
   if (player.replicanti.unl && player.replicanti.amount.gt(1)) {
